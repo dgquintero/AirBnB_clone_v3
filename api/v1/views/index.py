de6f870dccd46b2f329baa73e79b049d5e76682c
juanfe9118 <1168@holbertonschool.com>
2020-04-28 21:40:32 -0500

    Add stats api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,34 @@ Route that returns a JSON file with the status
 '''
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
 def display_status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def number_by_type():
+    """Returns the number of elements by type"""
+    amenities = storage.count(Amenity)
+    cities = storage.count(City)
+    places = storage.count(Place)
+    reviews = storage.count(Review)
+    states = storage.count(State)
+    users = storage.count(User)
+    return jsonify({
+        "amenities": amenities,
+        "cities": cities,
+        "places": places,
+        "reviews": reviews,
+        "states": states,
+        "users": users
+    })
